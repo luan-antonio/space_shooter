31fc1c478abe08b014792994675c88869d1d2d52
Luan Antonio <46446054+luan-antonio@users.noreply.github.com>
2024-11-29 09:09:41 -0300

    feat(app): Getting input

--- code/main.py
+++ code/main.py
@@ -26,7 +26,7 @@ meteor_rect = meteor_surf.get_frect(center=(WINDOW_WIDTH/2, WINDOW_HEIGHT/2))
 laser_surf = pygame.image.load(join('images', 'laser.png')).convert_alpha()
 laser_rect = laser_surf.get_frect(bottomleft=(20, WINDOW_HEIGHT - 20))
 
-player_direction = pygame.math.Vector2(1, 1)
+player_direction = pygame.math.Vector2(0, 0)
 player_speed = 300
 
 while running:
@@ -35,6 +35,19 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
+    # input
+    keys = pygame.key.get_pressed()
+    player_direction.x = int(keys[pygame.K_d]) - int(keys[pygame.K_a])
+    player_direction.y = int(keys[pygame.K_s]) - int(keys[pygame.K_w])
+    player_direction = player_direction.normalize(
+    ) if player_direction else player_direction
+    player_rect.center += player_direction * player_speed * dt
+
+    recent_keys = pygame.key.get_just_pressed()
+    if recent_keys[pygame.K_SPACE]:
+        print('fire laser')
+
     # draw the game
     display_surface.fill('darkgray')
     for pos in star_positions:
@@ -43,12 +56,6 @@ while running:
     display_surface.blit(source=meteor_surf, dest=meteor_rect)
     display_surface.blit(source=laser_surf, dest=laser_rect)
 
-    # player movement
-    if player_rect.bottom >= WINDOW_HEIGHT or player_rect.top <= 0:
-        player_direction.y *= -1
-    elif player_rect.right >= WINDOW_WIDTH or player_rect.left <= 0:
-        player_direction.x *= -1
-    player_rect.center += player_direction * player_speed * dt
     display_surface.blit(source=player_surf, dest=player_rect)
 
     pygame.display.update()
